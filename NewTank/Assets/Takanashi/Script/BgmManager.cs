2adadb92ad2b05370da3d9aa8f6b9a5fe11f8884
shinboriayanori <44427001+shinboriayanori@users.noreply.github.com>
2018-11-07 16:21:10 +0900

    高梨君のBGMスクリプト追加
    シーン跨いでもBGMを流し続けるスクリプト

---
+++ NewTank/Assets/Takanashi/Script/BgmManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BgmManager : MonoBehaviour {
+    public static BgmManager instance = null;
+
+    public AudioSource bgmAudio;
+
+    void Awake()
+    {
+        if (BgmManager.instance == null)
+        {
+            BgmManager.instance = this;
+        }
+        if (BgmManager.instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayBGM(AudioClip audioClip)
+    {
+        bgmAudio.clip = audioClip;
+        bgmAudio.Play();
+    }
+
+    public bool IsPlayingBGM()
+    {
+        return bgmAudio.isPlaying;
+    }
+}
